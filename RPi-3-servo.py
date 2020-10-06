78d295791cc91a7d717e212338f1d85d3b8be9df
jcondea <jcondea@gmail.com>
2020-10-06 15:51:15 -0300

    Update RPi-3-servo.py

--- RPi-3-servo.py
+++ RPi-3-servo.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+########################################################################
+# Filename    : RPi-3-servo.py
+# Description : Controla un servo motor. 
+# Author      : jcondea
+# modification: 2020/10/06
+########################################################################
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
@@ -8,7 +15,7 @@ y1=3.1
 x2=180
 y2=12.3
 for i in range(0,20):
-	desiredAngle=input("Angulo del servo? 0-180=")
+	desiredAngle=int(input("Angulo del servo? 0-180="))
         m=(y2-y1)/(x2-x1)
 	DC=m*(desiredAngle-x1)+y1
 	pwm.ChangeDutyCycle(DC)
